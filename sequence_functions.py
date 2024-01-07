d17e2d24b2507faa12637667271a42183a1dba63
butch0012 <elnathanmburu@gmail.com>
2024-01-07 18:17:49 +0300

    sequence

--- sequence_functions.py
+++ sequence_functions.py
@@ -1,3 +1,7 @@
 def remove_duplicates(sequence):
     seen = set()
     result = []
+    for item in sequence:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)

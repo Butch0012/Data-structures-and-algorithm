075077eee31764cf32df71d92e7514c1385b677b
butch0012 <elnathanmburu@gmail.com>
2024-01-07 17:30:53 +0300

    Add function is_balanced with stack-based approach

--- stack_functions.py
+++ stack_functions.py
@@ -1,3 +1,7 @@
 def is_balanced(expression):
     stack = []
     brackets = {'(': ')', '[': ']', '{': '}'}
+    for char in expression:
+        if char in brackets.keys():
+            stack.append(char)
+        elif char in brackets.values():

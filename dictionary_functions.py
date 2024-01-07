a2cc5ec51943fd47b095aea0202ea6657f2a9668
butch0012 <elnathanmburu@gmail.com>
2024-01-07 18:22:48 +0300

    Dictionary to store word frequencies

--- dictionary_functions.py
+++ dictionary_functions.py
@@ -2,3 +2,6 @@ import string
 
 def word_frequency(sentence):
      words = sentence.lower().translate(str.maketrans("", "", string.punctuation)).split()
+
+  # Dictionary to store word frequencies
+frequency_dict = {}
